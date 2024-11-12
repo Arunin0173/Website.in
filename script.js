5d73eb206021855bcf656953a4fe5e420bdeea77
Arunin0173 <arunrajraj0011@gmail.com>
2024-11-12 12:18:14 +0530

    Upload all file requerments

---
+++ script.js
@@ -0,0 +1,32 @@
+// function myMenufunction() {
+//     var menuBth = document.getElementById("myNavMenu");
+
+//     if (menuBth.className === "nav-menu") {
+//         menuBth.className += "reponsive";
+//     } else {
+//         menuBth.className = "nav-menu";
+//     }
+// }
+
+
+/*--------Dark mode------*/
+
+// const body = document.querySelector("body"),
+//     toggleSwitch = document.getElementById("toggle-switch");
+
+//     toggleSwitch.addEventListner("click", ()=> {
+//         body.classList.toggle("dark");
+//     });
+
+ /*---------Typing Effect---------*/
+
+    // var TypingEffect = new Typed(".typingText", {
+    //     strings: ["Frontend Devloper", "Designer", "App Devloper"],
+
+    //     loop: true,
+    //     typeSpeed: 100,
+    //     backSpeed: 80,
+    //     backDelay: 2000,
+    // })
+
+    
